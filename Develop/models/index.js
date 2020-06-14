e1372eabe6028437648475666eba009e7060ca9e
BrandonKutivan <bskutivan@gmail.com>
2020-06-14 12:10:58 -0700

    added table relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsToMany(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.haveMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
